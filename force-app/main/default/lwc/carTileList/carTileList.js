d984bdf8a6f9578f0da8f9f0e55c99ad30407d63
abhi070301 <abhishekbairwa373@gmail.com>
2024-05-11 10:37:50 +0530

    commit

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -2,8 +2,9 @@ import { LightningElement,wire } from 'lwc';
 import getCars from '@salesforce/apex/CarController.getCars'
 export default class CarTileList extends LightningElement {
     cars
-
-    @wire(getCars)
+    error
+    filters = {}
+    @wire(getCars, {filters: '$filters'})
     carsHandler({data, error}){
         if(data){
             console.log(data)
